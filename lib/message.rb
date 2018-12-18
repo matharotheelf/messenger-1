079f0f7a2ba1f0ddb5da67bb6f1d231d22e8a740
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2018-12-18 10:12:51 +0000

    refactored unit tests for message class, still on 3rd feature test

--- lib/message.rb
+++ lib/message.rb
@@ -1,7 +1,9 @@
 class Message
 
-def write(message)
-	message
+attr_reader :message_contents
+
+def initialize(message)
+  @message_contents = message
 end
 
 end
--- spec/unit_tests/message_spec.rb
+++ spec/unit_tests/message_spec.rb
@@ -3,14 +3,10 @@ require './lib/message'
 describe Message do
 
   let (:message_text) { double(:message_text) }
-  let (:message) { Message.new }
+  let (:message) { Message.new(message_text) }
   
-    it 'responds to #write' do
-      expect(message).to respond_to(:write).with(1)
-      # .write(message_text)).to eq(message)
+    it 'stores text' do
+      expect(message.message_contents).to eq(message_text)
     end
 
-    it '#write stores text' do
-      expect(message.write(message_text)).to eq(message_text)
-    end
 end 
