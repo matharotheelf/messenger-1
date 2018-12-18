a9a15eaffa7945c172beb3ac00aa327d0087ee0b
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2018-12-18 10:04:47 +0000

    3rd feature test - 5th unit test passed

--- lib/message.rb
+++ lib/message.rb
@@ -1,6 +1,7 @@
 class Message
 
 def write(message)
+	message
 end
 
 end
--- spec/unit_tests/message_spec.rb
+++ spec/unit_tests/message_spec.rb
@@ -5,8 +5,12 @@ describe Message do
   let (:message_text) { double(:message_text) }
   let (:message) { Message.new }
   
-    it 'stores a message' do
+    it 'responds to #write' do
       expect(message).to respond_to(:write).with(1)
       # .write(message_text)).to eq(message)
+    end
+
+    it '#write stores text' do
+      expect(message.write(message_text)).to eq(message_text)
     end
 end 
