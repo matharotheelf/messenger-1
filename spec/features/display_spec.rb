b8186de39c083dbfc2a70ce542cfaac4945637e4
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2018-12-19 15:08:09 +0000

    Functioning link

--- spec/features/display_spec.rb
+++ spec/features/display_spec.rb
@@ -1,12 +1,18 @@
-message = "Message to test only display first 20 chars"
-
 feature 'User can see message summary' do
   scenario "User only sees message's first 20 chars" do
     visit ('/')
-    fill_in 'message', with: message
-    click_button 'Submit'
     fill_in 'message', with: 'jack sparrow davey jones orlando bloom pirates'
     click_button 'Submit'
     expect(page).to_not have_content 'ones orlando bloom pirates'
+  end
+end
+
+feature 'User can press on button to see full message' do
+  scenario "User can press button to open page with full message" do
+    visit('/')
+    fill_in 'message', with: 'jack sparrow davey jones orlando bloom pirates'
+    click_button 'Submit'
+    #fill_in 'message', with: 'I am jack sparrow You are Davey Jones'
+    click_link 'jack sparrow davey j'
   end
 end
